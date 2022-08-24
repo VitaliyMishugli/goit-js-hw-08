1bbfab5327947b15ec6ef743c40666d811d57d30
Vitaliy Mishugli <vmishughli@gmail.com>
2022-08-24 20:55:05 +0100

    commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,45 @@
+import SimpleLightbox from 'simplelightbox/dist/simple-lightbox.esm';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 // console.log(galleryItems);
-console.log(22222);
 
+const gallery = document.querySelector('.gallery');
+
+const makeGalleryItemMarkup = ({ original, preview, description }) => {
+  return ` 
+  <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>
+  `;
+};
+
+const makeGalleryMarkup = galleryItems.map(makeGalleryItemMarkup).join('');
+gallery.insertAdjacentHTML('beforeend', makeGalleryMarkup);
+
+gallery.addEventListener('click', e => {
+  e.preventDefault();
+  //Створюємо модалку для одного зображення
+  const instance = basicLightbox.create(`
+      <img src="${e.target.src}" width="800" height="600">
+  `);
+
+  instance.show();
+  if (instance.visible()) {
+    window.addEventListener('keyup', e => {
+      console.log(e.code);
+      if (e.code === 'Escape') {
+        instance.close();
+      }
+    });
+  }
+});
