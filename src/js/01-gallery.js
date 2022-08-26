81947f8b4d58ea5d3c4a05f851dc742da9146625
Vitaliy Mishugli <vmishughli@gmail.com>
2022-08-26 21:18:39 +0100

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,26 +2,16 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 // import * as basicLightbox from 'basiclightbox';
 
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
-// console.log(galleryItems);
 
 const gallery = document.querySelector('.gallery');
 
 const makeGalleryItemMarkup = ({ original, preview, description }) => {
-  return ` 
-  <div class="gallery__item">
-    <a class="gallery__link" href="${original}">
-      <img
-        class="gallery__image"
-        src="${preview}"
-        data-source="${original}"
-        alt="${description}"
-      />
+  return `
+    <a href="${original}" class="gallery__item">
+      <img class="gallery__image" src="${preview}" alt="${description}" title="The nice picture"/>
     </a>
-  </div>
   `;
 };
 
@@ -30,12 +20,6 @@ gallery.insertAdjacentHTML('beforeend', makeGalleryMarkup);
 
 gallery.addEventListener('click', e => {
   e.preventDefault();
-  //Створюємо модалку для одного зображення
-  const instance = basicLightbox.create(`
-      <img src="${e.target.src}" width="800" height="600">
-  `);
-
-  instance.show();
   if (instance.visible()) {
     window.addEventListener('keyup', e => {
       console.log(e.code);
@@ -44,12 +28,64 @@ gallery.addEventListener('click', e => {
       }
     });
   }
-
-  var lightbox = new SimpleLightbox('.gallery a', {
-    captionsData: 'alt',
-    captionDelay: 250,
-  });
 });
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+
+// ==============
+
+// // Add imports above this line
+// import { galleryItems } from './gallery-items';
+// // Change code below this line
+
+// // console.log(galleryItems);
+
+// const gallery = document.querySelector('.gallery');
+
+// const makeGalleryItemMarkup = ({ original, preview, description }) => {
+//   return ` 
+//   <div class="gallery__item">
+//     <a class="gallery__link" href="${original}">
+//       <img
+//         class="gallery__image"
+//         src="${preview}"
+//         data-source="${original}"
+//         alt="${description}"
+//       />
+//     </a>
+//   </div>
+//   `;
+// };
+
+// const makeGalleryMarkup = galleryItems.map(makeGalleryItemMarkup).join('');
+// gallery.insertAdjacentHTML('beforeend', makeGalleryMarkup);
+
+// gallery.addEventListener('click', e => {
+//   e.preventDefault();
+//   //Створюємо модалку для одного зображення
+//   const instance = basicLightbox.create(`
+//       <img src="${e.target.src}" width="800" height="600">
+//   `);
+
+//   instance.show();
+//   if (instance.visible()) {
+//     window.addEventListener('keyup', e => {
+//       console.log(e.code);
+//       if (e.code === 'Escape') {
+//         instance.close();
+//       }
+//     });
+//   }
+
+//   var lightbox = new SimpleLightbox('.gallery a', {
+//     captionsData: 'alt',
+//     captionDelay: 250,
+//   });
+// });
 
 
 
