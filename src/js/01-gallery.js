632dbfe507c28fe9ba61b2ace889abf72290dfd2
Vitaliy Mishugli <vmishughli@gmail.com>
2022-08-26 20:45:57 +0100

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,6 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+// import * as basicLightbox from 'basiclightbox';
 
 // Add imports above this line
 import { galleryItems } from './gallery-items';
