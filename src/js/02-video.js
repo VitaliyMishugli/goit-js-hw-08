17c3dc4e411258b59be2976c476ae2576bb55469
Vitaliy Mishugli <vmishughli@gmail.com>
2022-08-27 15:31:22 +0100

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,29 +1,29 @@
 import Player from '@vimeo/player';
 
- const options = {
-   id: 59777392,
-   width: 640,
-   loop: true,
- };
+//  const options = {
+//    id: 59777392,
+//    width: 640,
+//    loop: true,
+//  };
 
- const player = new Player('handstick', {
-   id: 19231868,
-   width: 640,
- });
+//  const player = new Player('handstick', {
+//    id: 19231868,
+//    width: 640,
+//  });
 
- player.on('play', function () {
-   console.log('played the video!');
- });
+//  player.on('play', function () {
+//    console.log('played the video!');
+//  });
 
 
 // ============
-// const iframe = document.querySelector('iframe');
-// const player = new Vimeo.Player(iframe);
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
 
-// player.on('play', function () {
-//   console.log('played the video!');
-// });
+player.on('play', function () {
+  console.log('played the video!');
+});
 
-// player.getVideoTitle().then(function (title) {
-//   console.log('title:', title);
-// });
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
