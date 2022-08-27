76e1edf0833171bacd42c2098726b449baa3f08f
Vitaliy Mishugli <vmishughli@gmail.com>
2022-08-27 14:57:27 +0100

    2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,27 @@
 import Player from '@vimeo/player';
 
-const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+ const options = {
+   id: 59777392,
+   width: 640,
+   loop: true,
+ };
 
-player.on('play', function () {
-  console.log('played the video!');
-});
+ const player = new Vimeo.Player('made-in-ny', options);
 
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
+ player.setVolume(0);
+
+ player.on('play', function () {
+   console.log('played the video!');
+ });
+
+// ============
+// const iframe = document.querySelector('iframe');
+// const player = new Vimeo.Player(iframe);
+
+// player.on('play', function () {
+//   console.log('played the video!');
+// });
+
+// player.getVideoTitle().then(function (title) {
+//   console.log('title:', title);
+// });
