278ad0821049a4fa4f39376545c8ca4859475e43
Vitaliy Mishugli <vmishughli@gmail.com>
2022-08-29 18:33:16 +0100

    3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,20 @@
+import throttle from 'lodash.throttle';
+const email = document.querySelector('input');
+const textarea = document.querySelector('textarea');
+const submit = document.querySelector('.submit');
 
+email.addEventListener('input', onEmailInput);
+submit.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+}
+
+
+function onEmailInput(evt) {
+  const value = evt.target.value;
+  console.log(value);
+  localStorage.setItem('value', value);
+}
+
+function onTextAreaInput(evt){}
